f9bdf3975d71d605e1b24236da4814dc01efbd61
keruzam <mazurek.zg@gmail.com>
2018-07-08 15:41:37 +0200

    @Controller to @RestController

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,21 +2,19 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public Hello sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-        return new Hello(counter.incrementAndGet(), String.format(template, name));
-    }
+	@GetMapping("/hello")
+	public Hello sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+		return new Hello(counter.incrementAndGet(), String.format(template, name));
+	}
 
 }
